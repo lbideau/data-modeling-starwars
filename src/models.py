86b705fab1cb4957ef923d786d661494d13d717f
lbideau <leshliebzt@gmail.com>
2021-10-29 20:56:28 +0000

    New Commit

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,26 +8,46 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Usuario(Base):
+    __tablename__ = 'Usuario'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
+    name = Column(String(250))
+    last_name = Column(String(10))
+    user_name = Column(String(30), unique=True)
+    favoritoUusario_id = Column(Integer,ForeignKey("FavoritoUsuario.id"))
+class FavoritoUsuario(Base):
+    __tablename__ = 'FavoritoUsuario'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    planeta_id = Column(Integer, ForeignKey("planeta.id"))
+    vehiculo_id = Column(Integer, ForeignKey("vehiculo.id"))
+    persona_id = Column(Integer, ForeignKey("personaje.id"))
+    usuario = relationship("Usuario", backref="FavoritoUsuario")
+   
+class Personaje(Base):
+    __tablename__= 'personaje'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(15),nullable=False, unique=True)
+    altura = Column(Float, nullable=False)
+    genero = Column(String(15), nullable=False)
+    favoritoUsuario= relationship("FavoritoUsuario", backref="personaje")
+class Planeta(Base):
+    __tablename__= "planeta"
+    id = Column(Integer, primary_key=True)
+    nombre_planeta = Column(String(15),nullable=False,unique=True)
+    periodo_rotacion = Column(Integer,nullable=False)
+    favoritoUsuario= relationship("FavoritoUsuario", backref="planeta")
 
-    def to_dict(self):
-        return {}
-
+class Vehiculo(Base):
+    __tablename__="vehiculo"
+    id = Column(Integer,primary_key=True)
+    nombre = Column(String(10),nullable=False,unique=True)
+    tipo = Column(String(10),nullable=False)
+    fabricante = Column(String(10),nullable=False)
+    longitud = Column(Integer,nullable=False)
+    favoritoUsuario= relationship("FavoritoUsuario", backref="vehiculo")
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
